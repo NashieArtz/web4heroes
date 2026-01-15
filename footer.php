dd9f0db6d169a9ba7b19ecbf2a18a855e09e3f3c
fallou thiaw <fadelthiaw7@gmail.com>
2026-01-15 14:07:00 +0100

    accueil.php

---
+++ footer.php
@@ -0,0 +1,52 @@
+<footer class="footer-custom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h5>Mention légale</h5>
+                <ul class="list-unstyled small">
+                    <li><a href="#" class="text-white-50">CGU</a></li>
+                    <li><a href="#" class="text-white-50">CGV</a></li>
+                    <li><a href="#" class="text-white-50">Utilisation des données</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Outils</h5>
+                <ul class="list-unstyled small">
+                    <li><a href="#" class="text-white-50">Bibliothèque de films</a></li>
+                    <li><a href="#" class="text-white-50">Déclaration d'incidents</a></li>
+                    <li><a href="#" class="text-white-50">Liste des super-vilains</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Contact</h5>
+                <p class="small text-white-50 mb-1">webheros@gmail.com</p>
+                <p class="small text-white-50 mb-1">Adresse postale 75000</p>
+                <p class="small text-white-50">+33 07 67 76 45 45</p>
+            </div>
+            <div class="col-md-3">
+                <h5>Suivez-nous</h5>
+                <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="text-white me-2"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <hr class="bg-white">
+        <div class="row align-items-center mt-4">
+            <div class="col-md-6">
+                <h6>Newsletter</h6>
+                <div class="input-group mb-3" style="max-width: 300px;">
+                    <input type="email" class="form-control form-control-sm" placeholder="exemple@email.com">
+                    <button class="btn btn-info btn-sm text-white" type="button">S'abonner</button>
+                </div>
+            </div>
+            <div class="col-md-6 text-end">
+                <img src="logo.png" alt="Logo" height="50">
+                <h5 class="d-inline ms-2">HEROS SOS <br><small>SAFETY FOR YOU</small></h5>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HEROS SOS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="#" alt="Logo" height="40"> HEROS SOS
+        </a>
+        <div class="collapse navbar-collapse justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Déclarer un incident</a></li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Rechercher
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Liste de héros</a></li>
+                        <li><a class="dropdown-item" href="#">Liste des vilains</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Liste des films</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button class="btn btn-primary btn-sm rounded-pill">Connexion</button>
+            <button class="btn btn-outline-light btn-sm rounded-pill">Inscription</button>
+        </div>
+    </div>
+</nav>
