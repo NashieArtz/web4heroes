1e6672ad848066f83651b17764e15223515cb31e
WU Ange <wu.ange@nfs.school>
2026-01-15 16:44:49 +0100

    feat: [Page Layout] & [New Files] - incident-list.php - global.css

--- config/routes.php
+++ config/routes.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Controllers\{
-    AuthController,
-    HomeController,
-};
+use App\Controllers\{AuthController, HomeController, IncidentController};
 use App\Core\Router;
 
 return function (Router $router) {
     $router->get('/', [HomeController::class, 'index']);
+    $router->get('/test', [IncidentController::class, 'index']);
 
     $router->get('/login', [AuthController::class, 'showLogin']);
     $router->post('/login', [AuthController::class, 'login']);
     $router->post('/logout', [AuthController::class, 'logout']);
+
+    $router->get('/incident-list', [IncidentController::class, 'showIncidents']);
 };
