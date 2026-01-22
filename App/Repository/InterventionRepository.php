7f9b4bc77c264c79226d369eb45dfc7b282e5511
WU Ange <wu.ange@nfs.school>
2026-01-22 14:46:30 +0100

    feat: InterventionRepository - createIntervention() - updateIntervention()

--- App/Repository/InterventionRepository.php
+++ App/Repository/InterventionRepository.php
@@ -2,13 +2,67 @@
 
 namespace App\Repository;
 
+use PDO;
+
 class InterventionRepository
 {
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     /**
-     * findAllWithHeroes()
-     * findByID()
-     * save() Créer/Mettre à jour
-     * updateStatus()
+     * @param array $data
+     * @return void
      */
+    public function createIntervention(array $data): void
+    {
+        $stmt = $this->pdo->prepare('INSERT INTO intervention (incidents_id, hero_profile_id)
+VALUES (:incidents_id, :hero_profile_id)');
+        $stmt->execute(['incidents_id' => $data['incidents_id'],
+            'hero_profile_id' => $data['hero_profile_id']]);
+    }
+
+    /**
+     * @return void
+     */
+    public function updateIntervention(array $data): void
+    {
+        $stmt = $this->pdo->prepare('UPDATE `intervention`
+SET `incidents_id` = :incidents_id, `hero_profile_id` = :hero_profile_id, `status` = :status, `date_close` = :date_close
+WHERE `id` = :id');
+        $stmt->execute(['incidents_id' => $data['incidents_id'],
+            'hero_profile_id' => $data['hero_profile_id'],
+            'status' => $data['status'],
+            'date_close' => $data['date_close'],
+            'id' => $data['id']]);
+
+    }
+
+    /**
+     * @return void
+     */
+    public function updateStatus(string $status): void
+    {
+
+    }
+
+    /**
+     * @return void
+     */
+    public function findByID(int $id): ?array
+    {
+
+    }
+
+    /**
+     * @return void
+     */
+    public function findByHero(string $hero): ?array
+    {
+
+    }
 
 }
